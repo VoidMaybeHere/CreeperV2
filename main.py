8232fce769322ffcae573032c2843b21c5f15511
VoidMaybeHere <landon.norman76898@gmail.com>
2025-02-21 13:39:48 -0600

    Add error detection and comments

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import bot
 import argparse
 
-def fail(): raise RuntimeError
+def fail(error):                                                                                                        #Error ""Handling""
+    print(error)    
+    raise RuntimeError                                                                                          
     
 
-parser = argparse.ArgumentParser("main.py")
-parser.add_argument("-t", help="Token of your discord bot, overrides token set in token.txt", type=str, required=False)
-args = parser.parse_args()
+parser = argparse.ArgumentParser("main.py") 
+parser.add_argument("-t", help="Token of your discord bot, overrides token set in token.txt", type=str, required=False) #Token override via -t
+args = parser.parse_args()                                                                                              #Parse commandline arguments
 
 if args.t == None:
     try:
-        file = open("token.txt", "r")
-        token = [line for line in file]
+        file = open("token.txt", "r") 
+        token = [line for line in file]                                                                                 #Get all lines in file
         file.close()
         token = token[0]
         token = token.strip()
-    except FileExistsError: fail()
-    except FileNotFoundError: fail()
+    except FileExistsError: fail(FileExistsError)
+    except FileNotFoundError: fail(FileNotFoundError)
+    except IndexError: fail(IndexError)
 
-else: 
+else:                                                                                                                   #Format token string
     token = args.t
     token = token.strip()
         
-print(token)
 
 def main(token):
     bot.run(token)
