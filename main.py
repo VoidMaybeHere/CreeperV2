5a794189539d08282d6e7e675b2c947e42f52ab3
VoidMaybeHere <landon.norman76898@gmail.com>
2025-02-21 13:17:53 -0600

    Added commandline argument parsing Added Token file parsing

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-import discord.py
+import bot
+import argparse
+
+def fail(): raise RuntimeError
+    
+
+parser = argparse.ArgumentParser("main.py")
+parser.add_argument("-t", help="Token of your discord bot, overrides token set in token.txt", type=str, required=False)
+args = parser.parse_args()
+
+if args.t == None:
+    try:
+        file = open("token.txt", "r")
+        token = [line for line in file]
+        file.close()
+        token = token[0]
+        token = token.strip()
+    except FileExistsError: fail()
+    except FileNotFoundError: fail()
+
+else: 
+    token = args.t
+    token = token.strip()
+        
+print(token)
+
+def main(token):
+    bot.run(token)
+
