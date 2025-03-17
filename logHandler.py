ebde42e9f319bfd3c4d834d3383ac7ba36f9cc4b
VoidMaybeHere <landon.norman76898@gmail.com>
2025-03-17 12:37:09 -0500

    Fix for prev commit

--- logHandler.py
+++ logHandler.py
@@ -1,4 +1,5 @@
 import logging
+import logging.handlers
 
 class logHandler:
     def genLogger(self, logName, logger=None):
@@ -7,12 +8,14 @@ class logHandler:
         logDir = "./data/log/"+logName+".log"
         dt_fmt = '%Y-%m-%d %H:%M:%S'
         Log = logging.getLogger(logger)
+        
         handler = logging.handlers.RotatingFileHandler(
             filename=logDir,
             encoding='utf-8',
             maxBytes=32 * 1024 * 1024,  # 32 MiB
             backupCount=5,  # Rotate through 5 files
             )
+        
         Log.addHandler(logging.StreamHandler().setFormatter(formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')))
         Log.addHandler(handler.setFormatter(formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')))
         Log.setLevel(logging.INFO)
