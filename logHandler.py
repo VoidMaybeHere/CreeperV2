57d6408979213eb34a99b868d7f9f23dec60356d
VoidMaybeHere <landon.norman76898@gmail.com>
2025-03-17 12:32:36 -0500

    Change log handler to rotating file handler

--- logHandler.py
+++ logHandler.py
@@ -1,12 +1,20 @@
 import logging
 
 class logHandler:
-    def genLogger(self, logName):
+    def genLogger(self, logName, logger=None):
+        if logger == None:
+            logger = logName
         logDir = "./data/log/"+logName+".log"
         dt_fmt = '%Y-%m-%d %H:%M:%S'
-        Log = logging.getLogger(logName)
+        Log = logging.getLogger(logger)
+        handler = logging.handlers.RotatingFileHandler(
+            filename=logDir,
+            encoding='utf-8',
+            maxBytes=32 * 1024 * 1024,  # 32 MiB
+            backupCount=5,  # Rotate through 5 files
+            )
         Log.addHandler(logging.StreamHandler().setFormatter(formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')))
-        Log.addHandler(logging.FileHandler(filename=logDir, mode="a", encoding="utf-8").setFormatter(formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')))
+        Log.addHandler(handler.setFormatter(formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')))
         Log.setLevel(logging.INFO)
         Log.info(logName+" Logger Initalized")
         self._logger = Log
