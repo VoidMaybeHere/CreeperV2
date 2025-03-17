107ff68c69f230c77239c9fc6625e6c4bf522789
VoidMaybeHere <landon.norman76898@gmail.com>
2025-03-17 12:27:39 -0500

    Standardize format for logging

--- logHandler.py
+++ logHandler.py
@@ -3,8 +3,9 @@ import logging
 class logHandler:
     def genLogger(self, logName):
         logDir = "./data/log/"+logName+".log"
+        dt_fmt = '%Y-%m-%d %H:%M:%S'
         Log = logging.getLogger(logName)
-        Log.addHandler(logging.StreamHandler())
+        Log.addHandler(logging.StreamHandler().setFormatter(formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')))
         Log.addHandler(logging.FileHandler(filename=logDir, mode="a", encoding="utf-8").setFormatter(formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')))
         Log.setLevel(logging.INFO)
         Log.info(logName+" Logger Initalized")
